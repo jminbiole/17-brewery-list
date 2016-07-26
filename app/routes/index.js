8a6182863d13b8e46efa50a25fd61d79a7424bdc
Jeff Minbiole <minbiole.jeff@gmail.com>
2016-07-26 15:06:26 -0500

    made layout more dynamic

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,6 +5,5 @@ export default Ember.Route.extend({
   model() {
     return fetch(`http://tiy-brewery-proxy.herokuapp.com/v2/breweries?key=${config.apiKey}&established=2015`)
       .then((res) => res.json());
-      debugger;
   }
 });
