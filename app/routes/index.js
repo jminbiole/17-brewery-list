c52b640c9ae1996f72f73b6126bd2e78d33fdae6
Jeff Minbiole <minbiole.jeff@gmail.com>
2016-07-26 13:58:33 -0500

    fixing responsive grid

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,6 @@ import config from '../config/environment';
 
 export default Ember.Route.extend({
   model() {
-        // return fetch('https://api.imgur.com/3/gallery/hot/viral/0.json')
     return fetch(`http://tiy-brewery-proxy.herokuapp.com/v2/breweries?key=${config.apiKey}&established=2015`)
       .then((res) => res.json());
       debugger;
